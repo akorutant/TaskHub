445d59c4b3b3fdfb86cb9326f80a2d225678441b
xtern0o <dos30028@gmail.com>
2024-05-11 19:00:43 +0300

    [feat] refactored frontend in create task activity

--- app/src/main/java/com/project/taskhub2/CreateTaskActivity.java
+++ app/src/main/java/com/project/taskhub2/CreateTaskActivity.java
@@ -7,15 +7,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.util.ArrayList;
+
 public class CreateTaskActivity extends AppCompatActivity {
     EditText taskTitle, taskDescription;
+
+    ArrayList<State> states;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_task);
-        taskTitle = findViewById(R.id.taskTitleEdit);
-        taskDescription = findViewById(R.id.taskDescriptionEdit);
+        taskTitle = findViewById(R.id.taskTitle);
+        taskDescription = findViewById(R.id.taskDescription);
         Button submit = findViewById(R.id.submitButton);
 
         submit.setOnClickListener(new View.OnClickListener() {
