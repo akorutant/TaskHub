fc43bb21fae95df8ff1ce999ef2e050ce323f50e
xtern0o <dos30028@gmail.com>
2024-06-04 19:44:25 +0300

    [feat] bundle in group fragment

--- app/src/main/java/com/project/taskhub2/GroupFragment.java
+++ app/src/main/java/com/project/taskhub2/GroupFragment.java
@@ -13,6 +13,7 @@ import android.widget.TextView;
 
 import com.google.android.material.tabs.TabLayout;
 
+import java.util.ArrayList;
 import java.util.Objects;
 
 
@@ -21,6 +22,8 @@ public class GroupFragment extends Fragment {
     TabLayout tabLayout;
     FrameLayout frameLayout;
     TextView groupName;
+
+    ArrayList<User> users;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -32,10 +35,23 @@ public class GroupFragment extends Fragment {
         if (bundle != null) {
             groupName = v.findViewById(R.id.group_name_tv);
             groupName.setText(Objects.requireNonNull(bundle.get("groupName")).toString());
+            ArrayList<String> userIds = (ArrayList<String>) bundle.get("usersId");
+
+            Bundle bundle1 = new Bundle();
+            bundle1.putStringArrayList("userIds", userIds);
         }
 
-        // TODO: сделать логику переключения pager между фрагментами ок там на самом деле 80% простые но за 2 дня вряд ли упеешь все разобрать
+        // TODO: сделать логику переключения pager между фрагментами
 
         return v;
+    }
+
+    private void setInitialData() {
+        User user1 = new User("1", "user1", "mail@mail.ma");
+        User user2 = new User("2", "user222", "maiffl@madfsil.ma");
+        User user3 = new User("3", "user33333", "fdfd@mafdsfil.ma");
+        users.add(user1);
+        users.add(user2);
+        users.add(user3);
     }
 }
