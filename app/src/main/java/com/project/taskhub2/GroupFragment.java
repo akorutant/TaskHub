2e58317a5dcc65622604d5ab13c9c947b727fc46
xtern0o <dos30028@gmail.com>
2024-06-04 14:02:23 +0300

    [to merge]

--- app/src/main/java/com/project/taskhub2/GroupFragment.java
+++ app/src/main/java/com/project/taskhub2/GroupFragment.java
@@ -14,7 +14,7 @@ public class GroupFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_group, container, false);
     }
 }
