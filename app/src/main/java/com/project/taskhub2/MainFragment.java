db9855b50d34a07401a2b798512317c553c241fb
akorutant <akorutant@mail.ru>
2024-06-05 14:32:30 +0300

    add show only user task in main

--- app/src/main/java/com/project/taskhub2/MainFragment.java
+++ app/src/main/java/com/project/taskhub2/MainFragment.java
@@ -37,8 +37,7 @@ public class MainFragment extends Fragment {
 
         View v = inflater.inflate(R.layout.fragment_main, container, false);
 
-        setInitialData();
-        getNotCompletedTasks();
+        getUserTasks();
         recyclerView = v.findViewById(R.id.recycleView);
         taskAdapter = new TaskAdapter(this.getContext(), tasks);
         recyclerView.setLayoutManager(new LinearLayoutManager(this.getContext()));
@@ -69,63 +68,25 @@ public class MainFragment extends Fragment {
 
     }
 
-    private void getNotCompletedTasks() {
+    private void getUserTasks() {
         String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
-        DatabaseReference groupRef = FirebaseDatabase.getInstance().getReference("Group");
-        groupRef.addListenerForSingleValueEvent(new ValueEventListener() {
+        DatabaseReference taskRef = FirebaseDatabase.getInstance().getReference("Task");
+        Query userTasksQuery = taskRef.orderByChild("worker/id").equalTo(uid);
+        userTasksQuery.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                ArrayList<Group> groups = new ArrayList<>();
-                for (DataSnapshot groupSnapshot : dataSnapshot.getChildren()) {
-                    Group group = groupSnapshot.getValue(Group.class);
-                    if (group != null) {
-                        boolean userIsMember = false;
-                        ArrayList<User> users = new ArrayList<>();
-                        for (DataSnapshot userSnapshot : groupSnapshot.child("users").getChildren()) {
-                            User user = userSnapshot.getValue(User.class);
-                            if (user != null) {
-                                if (user.getId().equals(uid)) {
-                                    userIsMember = true;
-                                }
-                                users.add(user);
-                            }
-                        }
-                        if (userIsMember) {
-                            group.setUsers(users);
-                            groups.add(group);
-                        }
+                ArrayList<Task> userTasks = new ArrayList<>();
+                for (DataSnapshot taskSnapshot : dataSnapshot.getChildren()) {
+                    Task task = taskSnapshot.getValue(Task.class);
+                    if (task != null) {
+                        userTasks.add(task);
                     }
                 }
 
-                DatabaseReference taskRef = FirebaseDatabase.getInstance().getReference("Task");
-                Query notCompletedTasksQuery = taskRef.orderByChild("completed").equalTo(false);
-                notCompletedTasksQuery.addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        ArrayList<Task> notCompletedTasks = new ArrayList<>();
-                        for (DataSnapshot taskSnapshot : dataSnapshot.getChildren()) {
-                            Task task = taskSnapshot.getValue(Task.class);
-                            if (task != null) {
-                                for (Group group : groups) {
-                                    if (group.getId().equals(task.getGroup().getId())) {
-                                        notCompletedTasks.add(task);
-                                        break;
-                                    }
-                                }
-                            }
-                        }
-
-                        // Обновляем список tasks и вызываем notifyDataSetChanged() у адаптера
-                        tasks.clear();
-                        tasks.addAll(notCompletedTasks);
-                        taskAdapter.notifyDataSetChanged();
-                    }
-
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
-                        // Обрабатываем ошибку
-                    }
-                });
+                // Обновляем список tasks и вызываем notifyDataSetChanged() у адаптера
+                tasks.clear();
+                tasks.addAll(userTasks);
+                taskAdapter.notifyDataSetChanged();
             }
 
             @Override
